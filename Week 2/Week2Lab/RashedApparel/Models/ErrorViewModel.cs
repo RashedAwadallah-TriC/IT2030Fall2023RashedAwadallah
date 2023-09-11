574decd3d5828382fcf059f82e0b81c41c864db4
awadallah.rashed01@gmail.com <awadallah.rashed01@gmail.com>
2023-09-10 21:38:31 -0400

    completed lab 2

---
+++ Week 2/Week2Lab/RashedApparel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RashedApparel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
