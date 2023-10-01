f6ec40fcb49d074215b7410c00986a7a76311f31
awadallah.rashed01@gmail.com <awadallah.rashed01@gmail.com>
2023-10-01 18:01:53 -0400

    finished Lab 5

---
+++ Week 5/RashedApparel/Models/ContactModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RashedApparel.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        //The [Phone] validator wasn't good enough, in my opinion
+        [RegularExpression(@"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$",
+         ErrorMessage = "Please enter a valid phone number")]
+        public string? Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Message { get; set; }
+    }
+}
---
+++ Week 5/readme.md
@@ -0,0 +1,3 @@
+## Week 5 Lab
+
+##### I added 2 new views (About and Contact), as well as a new Contact controller and Contact model. I also modified the existing code (from Week 2) to change the Nav Bar's color and to update the navigation links. I also added a "Welcome" header to the home page.
